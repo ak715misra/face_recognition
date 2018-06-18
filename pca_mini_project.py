38ef7c9a55a9ef99ccbbd14e9917d002d6149f24
Ajay K. Misra <akmisra@ptd.net>
2018-06-18 17:53:23 -0400

    feat: Using PCA reduce the features to recognize the faces.
    This project uses eigenfaces, SVMs and PCA to recognize the faces.

--- pca_mini_project.py
+++ pca_mini_project.py
@@ -1,20 +1,10 @@
 
 # coding: utf-8
-
-# 
 # # PCA Mini-Project 
 # ### Faces recognition example using eigenfaces and SVMs
-# Our discussion of PCA spent a lot of time on theoretical issues, so in this mini-project we’ll ask you to play around with some sklearn code. The eigenfaces code is interesting and rich enough to serve as the testbed for this entire mini-project.
-# 
-# 
-# 
 # Note:
 # The dataset used in this example is a preprocessed excerpt of the
 # "[Labeled Faces in the Wild](http://vis-www.cs.umass.edu/lfw/)", aka [LFW_ Download](http://vis-www.cs.umass.edu/lfw/lfw-funneled.tgz) (233MB). [Original source](http://scikit-learn.org/stable/auto_examples/applications/face_recognition.html).
-
-# In[54]:
-
-
 
 from time import time
 import logging
@@ -30,11 +20,7 @@ from sklearn.decomposition import RandomizedPCA
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 
-
 # ## Loading the dataset
-
-# In[55]:
-
 
 # Download the data, if not already on disk and load it as numpy arrays
 lfw_people = fetch_lfw_people('data', min_faces_per_person=70, resize=0.4)
@@ -42,7 +28,6 @@ lfw_people = fetch_lfw_people('data', min_faces_per_person=70, resize=0.4)
 # introspect the images arrays to find the shapes (for plotting)
 n_samples, h, w = lfw_people.images.shape
 np.random.seed(42)
-
 
 # for machine learning we use the data directly (as relative pixel
 # position info is ignored by this model)
@@ -59,21 +44,12 @@ print("n_samples: %d" % n_samples)
 print("n_features: %d" % n_features)
 print( "n_classes: %d" % n_classes)
 
-
 # ### Split into a training and testing set
-
-# In[56]:
-
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
-
 
 # ## Compute PCA
 # 
 # We can now compute a [PCA](http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html) (eigenfaces) on the face dataset (treated as unlabeled dataset): unsupervised feature extraction / dimensionality reduction.
-
-# In[57]:
-
 
 n_components = 150
 #n_components = [10, 15, 25, 50, 100, 250, 300]
@@ -87,14 +63,9 @@ pca = PCA(n_components=n_components, whiten=True, svd_solver='randomized')
 #TODO: pass the training dataset (X_train) to pca's 'fit()' method
 pca = pca.fit(X_train)
 
-
 print("done in %0.3fs" % (time() - t0))
 
-
 # Projecting the input data on the eigenfaces orthonormal basis
-
-# In[58]:
-
 
 eigenfaces = pca.components_.reshape((n_components, h, w))
 
@@ -104,14 +75,9 @@ X_test_pca = pca.transform(X_test)
 print("done in %0.3fs" % (time() - t0))
 #print(pca.explained_variance_ratio_)
 
-
 # ## Train a SVM classification model
 # 
 # Let's fit a [SVM classifier](http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html) to the training set. We'll use [GridSearchCV](http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html) to find a good set of parameters for the classifier.
-
-# In[59]:
-
-
 
 param_grid = {
          'C': [1e3, 5e3, 1e4, 5e4, 1e5],
@@ -125,34 +91,22 @@ clf = clf.fit(X_train_pca, y_train)
 print("Best estimator found by grid search:")
 print(clf.best_estimator_)
 
-
 # ## Evaluation of the model quality on the test set
 # 
 # #### 1. Classification Report
 # Now that we have the classifier trained, let's run it on the test dataset and qualitatively evaluate its results. Sklearn's [classification_report](http://scikit-learn.org/stable/modules/generated/sklearn.metrics.classification_report.html) shows some of the main classification metrics for each class.
 
-# In[60]:
-
-
 y_pred = clf.predict(X_test_pca)
 
 print(classification_report(y_test, y_pred, target_names=target_names))
-
 
 # #### 2. Confusion Matrix
 # 
 # Another way to look at the performance of the classifier is by looking the [confusion matrix](http://www.dataschool.io/simple-guide-to-confusion-matrix-terminology/). We can do that by simply invoking [sklearn.metrics.confusion_matrix](http://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html):
 
-# In[61]:
-
-
 print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
 
-
 # #### 3. Plotting The Most Significant Eigenfaces
-
-# In[62]:
-
 
 def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
     """Helper function to plot a gallery of portraits"""
@@ -164,8 +118,6 @@ def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
         pl.title(titles[i], size=12)
         pl.xticks(())
         pl.yticks(())
-
-
 
 # plot the result of the prediction on a portion of the test set
 
@@ -181,15 +133,10 @@ plot_gallery(X_test, prediction_titles, h, w)
 
 pl.show()
 
-
-# In[63]:
-
-
 eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
 plot_gallery(eigenfaces, eigenface_titles, h, w)
 
 pl.show()
-
 
 # ## Quiz: Explained Variance Of Each PC
 # 
@@ -197,12 +144,8 @@ pl.show()
 # 
 # 
 
-# In[12]:
-
-
-Variance explained by the first PCA: 0.19346534 or 19.34%
-Variance explained by the 2nd PCA: 0.1511682 or 15.11%
-
+# Variance explained by the first PCA: 0.19346534 or 19.34%
+# Variance explained by the 2nd PCA: 0.1511682 or 15.11%
 
 # ## Quiz: How Many PCs To Use?
 # 
@@ -212,30 +155,19 @@ Variance explained by the 2nd PCA: 0.1511682 or 15.11%
 # 
 # As you add more principal components as features for training your classifier, do you expect it to get better or worse performance?
 
-# In[ ]:
-
-
-With the addition of PCs, we wee F1 score going higher means better performance but only upto a point e.g. 0.69 for 
-n_components=250. Addional PCs would not increase the F1 score.
-
+# With the addition of PCs, we wee F1 score going higher means better performance but only upto a point e.g. 0.69 for 
+# n_components=250. Addional PCs would not increase the F1 score.
 
 # ## Quiz: F1 Score Vs. No. Of PCs Used
 # 
 # Change n_components to the following values: [10, 15, 25, 50, 100, 250]. For each number of principal components, note the F1 score for Ariel Sharon. (For 10 PCs, the plotting functions in the code will break, but you should be able to see the F1 scores.) If you see a higher F1 score, does it mean the classifier is doing better, or worse?
 
-# In[ ]:
-
-
-We see F1 score going higher and that simply means classifier is doing better.
-
+# We see F1 score going higher and that simply means classifier is doing better.
 
 # ## Quiz: Dimensionality Reduction And Overfitting
 # Do you see any evidence of overfitting when using a large number of PCs? Does the dimensionality reduction of PCA seem to be helping your performance here?
 
-# In[ ]:
-
-
-Yes, We do notice overfitting. F1 score increases only up to a point. When number of PCs goes beyond 250, it starts going 
-down. Also dimensionality reduction is helping the performance because PCA is reducing the number of features from 1800 to 
-150 without losing much of the info. We notice the F1 score increasing during this process.
+# Yes, We do notice overfitting. F1 score increases only up to a point. When number of PCs goes beyond 250, it starts going 
+# down. Also dimensionality reduction is helping the performance because PCA is reducing the number of features from 1800 to 
+# 150 without losing much of the info. We notice the F1 score increasing during this process.
 
